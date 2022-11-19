2fa475003a787e4170060b52d7fb7d9f1cb6a352
sahil <sahilshityalkar05@gmail.com>
2022-11-19 22:59:27 +0530

    add input tag and get user choice store , create myFunction in RPS.js

--- RPS.js
+++ RPS.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+// console.log("hello world");
+
+function myFunction(){
+    var playerSelection = document.getElementById("mySelect").value;
+    document.getElementById("user-choice").innerHTML = "You selected: " + playerSelection;
+}
