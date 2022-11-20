cfc06c36bf7b386824b370b9fd6284bb2dea43a8
sahil <sahilshityalkar05@gmail.com>
2022-11-20 23:22:56 +0530

    add getCOmputerCHoice function for random selection

--- RPS.js
+++ RPS.js
@@ -1,6 +1,25 @@
 // console.log("hello world");
 
 function myFunction(){
-    var playerSelection = document.getElementById("mySelect").value;
+    let playerSelection = document.getElementById("mySelect").value;
     document.getElementById("user-choice").innerHTML = "You selected: " + playerSelection;
+    
+}
+
+let computerselection;
+
+function getComputerChoice(){
+    const randomeNumber = math.floor(math.randomeNumber() * 3 ) +1;
+
+    switch(randomeNumber){
+        case 1 :
+            computerselection = "rock";
+            break;
+        case 2 :
+            computerselection = "paper";
+            break;
+        case 3 :
+            computerselection = "Scissors";
+            break;
+    }
 }
