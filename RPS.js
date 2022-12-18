28a3bc63b944955471458ee577174e892ca50625
sahil <sahilshityalkar05@gmail.com>
2022-12-18 21:20:17 +0530

    remove a computersection algo 'try to solve error'

--- RPS.js
+++ RPS.js
@@ -5,21 +5,3 @@ function myFunction(){
     document.getElementById("user-choice").innerHTML = "You selected: " + playerSelection;
     
 }
-
-let computerselection;
-
-function getComputerChoice(){
-    const randomeNumber = math.floor(math.randomeNumber() * 3 ) +1;
-
-    switch(randomeNumber){
-        case 1 :
-            computerselection = "rock";
-            break;
-        case 2 :
-            computerselection = "paper";
-            break;
-        case 3 :
-            computerselection = "Scissors";
-            break;
-    }
-}
