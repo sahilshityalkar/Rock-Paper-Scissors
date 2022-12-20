7b7c439f6baf36c73933c7d97aa4bbb0612a6cf7
sahil <sahilshityalkar05@gmail.com>
2022-12-20 23:04:22 +0530

    adding a computerselection return value funcation and playRound function

--- RPS.js
+++ RPS.js
@@ -9,3 +9,38 @@ function myFunction(){
     document.getElementById("user-choice").innerHTML = "" + playerChoice;
     
 }
+let computerchoice;
+let playerscore = 0;
+let computerscore = 0;
+function getComputerChoice() {
+
+    let computerSelection = Math.floor((Math.random() * 3) + 1);
+
+    switch (computerSelection) {
+        case 1:
+            computerchoice = "rock"
+            break;
+        case 2:
+            computerchoice = "paper"
+            break;
+        case 3:
+            computerchoice = "scissors"
+            break;
+        default:
+            break;
+    }
+    switch (playRound()) {
+        case 1:
+            computerscore++;
+            document.getElementById("result").innerHTML = "result : Computer win the round";
+            break;
+        case 2:
+            playerscore++;
+            document.getElementById("result").innerHTML = "result : You win the round";
+            break;
+        case 3:
+            document.getElementById("result").innerHTML = "result : draw the match";
+        default:
+            break;
+    }
+}
