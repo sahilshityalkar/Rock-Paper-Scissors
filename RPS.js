737f92701ac1dafcbe9aec2ef94ead23ac7058d2
sahil <sahilshityalkar05@gmail.com>
2022-12-24 22:32:35 +0530

    add innerHTML tags showing choice in the screen

--- RPS.js
+++ RPS.js
@@ -60,4 +60,10 @@ function getComputerChoice() {
     else if(playerscore === 0 || computerscore === 0)(
         document.getElementById("final_result").innerHTML = "START THE MATCH"
     )
+
+    document.getElementById("computer-choice").innerHTML = ""+ computerchoice;
+    document.getElementById("result-user").innerHTML = + playerscore;
+    document.getElementById("result-computer").innerHTML = + computerscore;
+    playGround = playRound();
+    return computerchoice;
 }
